43e7cacb6e2803133f9fa3d2a6eff140d4a1980a
Fleetwood <wizening@gmail.com>
2018-04-13 02:57:07 -0500

    Twitter search and mostly UI

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const exec = require('child_process').exec;
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', () => gulp.src([
@@ -11,15 +12,29 @@ gulp.task('sass', () => gulp.src([
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', () => gulp.src([
-  'node_modules/bootstrap/dist/js/bootstrap.min.js',
   'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/tether/dist/js/tether.min.js',
+  'node_modules/bootstrap/dist/js/bootstrap.min.js',
+  'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  'node_modules/bootstratp/js/dist/alert.min.js',
+  'node_modules/bootstratp/js/dist/button.min.js',
+  'node_modules/bootstratp/js/dist/carousel.min.js',
+  'node_modules/bootstratp/js/dist/collapse.min.js',
+  'node_modules/bootstratp/js/dist/dropdown.min.js',
+  'node_modules/bootstratp/js/dist/index.min.js',
+  'node_modules/bootstratp/js/dist/modal.min.js',
+  'node_modules/bootstratp/js/dist/popover.min.js',
+  'node_modules/bootstratp/js/dist/scrollspy.min.js',
+  'node_modules/bootstratp/js/dist/tab.min.js',
+  'node_modules/bootstratp/js/dist/tooltip.min.js',
+  'node_modules/bootstratp/js/dist/util.min.js'
 ])
   .pipe(gulp.dest('public/js')));
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], () => {
   gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'public/scss/*.scss'], ['sass']);
+  exec('node ./bin/www');
+  console.log('Starting server....');
 });
 
 gulp.task('default', ['js', 'serve']);

a19d8773c5d4ec6e4c18404473785dba97669b36
Fleetwood <wizening@gmail.com>
2019-08-31 18:01:47 -0400

    Formattig in friTease.js

--- public/scripts/friTease.js
+++ public/scripts/friTease.js
@@ -2,21 +2,22 @@ $(document).ready(function () {
     const streamContent = $('#stream-content');
     const streamLoading = $('#stream-loading');
     let page = 0;
-    
+
     const urlParams = {};
-    new URLSearchParams(window.location.search).forEach((v,k) => urlParams[k]=v);
+    new URLSearchParams(window.location.search).forEach((v, k) => urlParams[k] = v);
 
     const render = (content, next = false) => {
         loadVisibility(false);
         if (next) {
             streamContent.append(content);
-            $('html, body').animate({scrollTop: $(`#page_${page}`).offset().top-50}, 800);
+            const top = $(`#page_${page}`).offset().top - 40;
+            $('html, body').animate({ scrollTop: top }, 800);
             return;
         }
         streamContent.html(content);
     }
 
-    const formats ={
+    const formats = {
         moment: 'YYYY-MM-DD',
         picker: 'yyyy-mm-dd'
     };
@@ -24,7 +25,7 @@ $(document).ready(function () {
     // because some tweets may have posted on Thursday
     const lastThursday = (yyyymmdd) => {
         let thurs = moment(yyyymmdd);
-        while(thurs.day() !== 4) {
+        while (thurs.day() !== 4) {
             thurs.add(-1, 'd');
         }
         return thurs.format(formats.moment);
@@ -32,11 +33,11 @@ $(document).ready(function () {
 
     const loadVisibility = (loading) => {
         const hidden = 'u-hiddenVisually';
-        const set = loading ? [streamLoading, streamContent] : [streamContent,streamLoading];
+        const set = loading ? [streamLoading, streamContent] : [streamContent, streamLoading];
         set[0].removeClass(hidden);
         set[1].addClass(hidden);
     }
-        
+
     const getStream = (next = false) => {
         loadVisibility(true);
         $.ajax({
